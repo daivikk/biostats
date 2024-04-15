0dd7efcb60a246539bb5a63894ec95e749079d96
Ashrith Athmaram <ashrithathmaram@gmail.com>
2024-04-15 19:16:00 -0400

    changed icon

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -88,9 +88,9 @@ export default function resultsPage() {
   <button onClick={save} className="mt-6 font-Inter font-medium relative bg-gradient-to-br from-blue to-red text-darkGray font-bold py-2 px-4 rounded-[8px] overflow-hidden w-[14rem] flex items-center">
     <span className="absolute inset-0 bg-gradient-to-br from-blue to-red"></span>
     <span className="relative z-10 font-Inter font-medium text-black flex items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"/>
-      </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
       Download Results.
     </span>
   </button>
